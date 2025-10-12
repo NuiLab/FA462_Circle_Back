23f5cd0ede465dc1c1aa9bf12b944e0aef147993
ConnorChap <connorchap2004@gmail.com>
2025-10-12 14:27:09 -0600

    door opens need to make text better

---
+++ Assets/Scripts/door/CameraOpenDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraDoorScript
+{
+	public class CameraOpenDoor : MonoBehaviour
+	{
+		public float DistanceOpen = 3f;
+		public GameObject text;
+
+		void Start()
+        {
+			text.SetActive(true);
+        }
+
+		void Update()
+		{
+			RaycastHit hit;
+			bool showText = false;
+
+			if (Physics.Raycast(transform.position, transform.forward, out hit, DistanceOpen))
+			{
+				var door = hit.transform.GetComponent<DoorScript.Door>();
+				if (door != null)
+				{
+					showText = true;
+					if (Input.GetKeyDown(KeyCode.E))
+						door.OpenDoor();
+				}
+			}
+
+			if (text.activeSelf != showText)
+				text.SetActive(showText);
+		}
+
+	}
+}
